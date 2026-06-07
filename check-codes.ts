1e7f381deee186fa934367d1c3831b462e980bfa
yuvraj001q <yuvraj001q@gmail.com>
2026-06-07 16:33:52 +0530

    Remove temp files

--- check-codes.ts
+++
@@ -1,4 +0,0 @@
-import { prisma } from './src/lib/prisma';
-const users = await prisma.user.findMany({ select: { email: true, referralCode: true } });
-console.log(JSON.stringify(users, null, 2));
-await prisma.$disconnect();
